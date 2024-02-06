ce583ec112906543de2c89d95b7449e5b68f56a4
Cesar Estiven Medrano <cesar-2007-mesa@hotmail.com>
2024-02-05 22:59:23 -0500

    Creando nueva anotacion

--- src/main/java/com/example/best_travel/aspects/NotifyAspect.java
+++ src/main/java/com/example/best_travel/aspects/NotifyAspect.java
@@ -1,18 +1,36 @@
 package com.example.best_travel.aspects;
 
+import com.example.best_travel.util.BestTravelUtil;
+import com.example.best_travel.util.annotations.Notify;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 @Component
 @Aspect
 public class NotifyAspect {
 
-    public void notifyInFile(JoinPoint joinPoint){
-        Arrays.stream(joinPoint.getArgs())
-                .forEach(System.out::println);
+    @After(value = "@annotation(com.example.best_travel.util.annotations.Notify)")
+    public void notifyInFile(JoinPoint joinPoint) throws IOException {
+
+        var args = joinPoint.getArgs();
+        var size = args[1];
+        var order = args[2] == null ? "None" :  args[2];
+        var text = String.format(LINE_FORMAT, LocalDateTime.now(), size.toString(), order.toString());
+
+        var signature = (MethodSignature) joinPoint.getSignature();
+        var method = signature.getMethod();
+        var annotation = method.getAnnotation(Notify.class);
+
+        BestTravelUtil.writeNotification(text, annotation.value());
     }
+
+    private static final String LINE_FORMAT = "At %s new request with size page %s add and order %s";
 
 }
--- src/main/java/com/example/best_travel/util/BestTravelUtil.java
+++ src/main/java/com/example/best_travel/util/BestTravelUtil.java
@@ -1,5 +1,8 @@
 package com.example.best_travel.util;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Random;
 
@@ -18,6 +21,17 @@ public class BestTravelUtil {
         int randomHours = random.nextInt(12 - 6) + 2;
         LocalDateTime now = LocalDateTime.now();
         return now.plusHours(randomHours);
+    }
+
+    public static void writeNotification(String text, String path) throws IOException {
+        var fileWriter = new FileWriter(path, true);
+        var bufferedWriter = new BufferedWriter(fileWriter);
+        try(fileWriter; bufferedWriter){
+            bufferedWriter.write(text);
+            bufferedWriter.newLine();
+        }
+
+
     }
 
 }
